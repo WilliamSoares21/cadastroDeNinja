00c4d2f3d21062c188903427bef008e768308699
William Soares <pedro.williamsoares2022@gmail.com>
2026-04-05 11:43:46 -0300

    feat: implement NinjaController CRUD endpoints #5

--- CadastroDeNinja/src/main/java/dev/java10x/CadastroDeNinja/Ninjas/NinjaController.java
+++ CadastroDeNinja/src/main/java/dev/java10x/CadastroDeNinja/Ninjas/NinjaController.java
@@ -1,28 +1,36 @@
 package dev.java10x.CadastroDeNinja.Ninjas;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/ninjas") // aqui é onde defino um caminho base para todos os endpoints de ninja
+@RequestMapping("/ninjas")
 public class NinjaController {
 
-  @Autowired
-  private NinjaService ninjaService;// aqui o spring vai injetar uma instância de NinjaService
+    private final NinjaService ninjaService;
 
-  @GetMapping("/boasvindas")
-  public String boasVindas() {
-    return "Boas Vindas a área de Ninjas";
-  }
+    public NinjaController(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
 
-  public ResponseEntity<NinjaModel> cadastrarNinja(@RequestBody NinjaModel ninja) {
-    NinjaModel novoNinja = ninjaService.cadastrarNinja(ninja);
-    return new ResponseEntity<>(novoNinja, HttpStatus.CREATED);
-  }
+    @GetMapping
+    public List<NinjaModel> listarNinjas() {
+        return ninjaService.listarNinjas();
+    }
 
+    @GetMapping("/{id}")
+    public NinjaModel buscarNinjaPorId(@PathVariable Long id) {
+        return ninjaService.buscarNinjaPorId(id).orElse(null);
+    }
+
+    @PostMapping
+    public NinjaModel cadastrarNinja(@RequestBody NinjaModel ninja) {
+        return ninjaService.cadastrarNinja(ninja);
+    }
+
+    @DeleteMapping("/{id}")
+    public void excluirNinja(@PathVariable Long id) {
+        ninjaService.excluirNinja(id);
+    }
 }
